3cc663b7e9723a273a11e93532180a875850dda1
cedricni <70257945+cedricni@users.noreply.github.com>
2023-09-28 05:56:41 -0400

    Delete Desgin directory

--- Desgin/src/com/cedric/CarService.java
+++
@@ -1,6 +0,0 @@
-package com.cedric;
-
-public interface CarService
-{
-    int eat(String e);
-}
--- Desgin/src/com/cedric/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.cedric;
-
-public interface UserService
-{
-    void logIn();
-    
-    void signUp();
-    
-}
